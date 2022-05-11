2213dd27a1a13aded3aa57a777f990598af7f25c
Arthur Zeng <zenghaoyang100@gmail.com>
2022-05-11 13:44:42 +0800

    upload lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -6,6 +6,10 @@ public class LeapYear {
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
+    static boolean isLeapYear(int year){
+        
+    }
+
     private static void checkLeapYear(int year) {
         if (isLeapYear(year)) {
             System.out.printf("%d is a leap year.\n", year);
